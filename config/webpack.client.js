bd38cb37a1430a06e8da71e0f49e5c4b91d1ec74
munaibh <munaibh@gmail.com>
2020-10-27 15:31:22 +0000

    We use svgs a lot, so the build should support them.

--- config/webpack.client.js
+++ config/webpack.client.js
@@ -35,7 +35,7 @@ module.exports = (env, _argv) => {
         exclude: /node_modules/,
         loader: 'babel-loader'
       }, {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(svg|png|jpe?g|gif)$/i,
         use: [{
           loader: 'file-loader',
           options: { name: `images/${filename}.[ext]`, publicPath: '/' }
